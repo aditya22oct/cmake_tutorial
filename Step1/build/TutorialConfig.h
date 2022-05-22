d7ee09615963e30434faeb8be2eec3668f4e7ba0
aditya22oct <aditya22oct@gmail.com>
2022-05-22 10:09:47 +0200

    deleted build folder

--- Step1/build/TutorialConfig.h
+++
@@ -1,6 +0,0 @@
-// the configured options and settings for Tutorial
-
-// between the @ symbol put project name 
-// the define doesn't really matter
-#define Tutorial_VERSION_MAJOR 1
-#define Tutorial_VERSION_MINOR 0
